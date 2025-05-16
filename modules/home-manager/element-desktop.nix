d441abaabcc5df8ed51f1ab0f0d1af77182e6156
SchweGELBin <abramjannikmichael06@gmail.com>
2025-05-16 21:06:15 +0200

    feat(home-manager/element-desktop): add accent color support
    pkgs/sources.json needs to be updated

--- modules/home-manager/element-desktop.nix
+++ modules/home-manager/element-desktop.nix
@@ -9,15 +9,20 @@ let
 in
 
 {
-  options.catppuccin.element-desktop = catppuccinLib.mkCatppuccinOption { name = "element-desktop"; };
+  options.catppuccin.element-desktop = catppuccinLib.mkCatppuccinOption {
+    name = "element-desktop";
+    accentSupport = true;
+  };
 
   config = lib.mkIf cfg.enable {
     programs.element-desktop = {
       settings = {
         default_theme =
-          "custom-Catppuccin " + (if cfg.flavor == "frappe" then "Frappé" else mkUpper cfg.flavor);
+          "custom-Catppuccin "
+          + (if cfg.flavor == "frappe" then "Frappé" else mkUpper cfg.flavor)
+          + " (${mkUpper cfg.accent})";
         setting_defaults.custom_themes = [
-          (lib.importJSON "${sources.element}/Catppuccin-${cfg.flavor}.json")
+          (lib.importJSON "${sources.element}/catppuccin-${cfg.flavor}/catppuccin-${cfg.flavor}-${cfg.accent}.json")
         ];
       };
     };
